5a48a446d7f385a2e835a56942e3e9084c3837a8
omars22 <104013312+omars22@users.noreply.github.com>
2022-05-11 17:21:59 +0400

    os first version

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Joloper_OS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Joloper OS 0.01.00";
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;                                                                                     ;
+
+            Console.WriteLine("JOLOPER INC. (C) V.0.01.00 All Rights Reserved.");
+            Console.WriteLine("JOLOPER OS, Write Your Username: ");
+            string userName = Console.ReadLine();
+            Console.Write("Hello " + userName + "! Write Your Password So We Can Start To Set Up Your Joloper: ");
+            string passWord = Console.ReadLine();
+            Console.WriteLine("Welcome, this might take a few minutes, make sure to follow what the terminal is saying.");
+            // Set Up Start
+            Console.ReadKey();
+        }
+    }
+}
